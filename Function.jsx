0ba3ac9df1dd5d9958fe2749f371726516f07b69
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-03-10 21:24:37 +0600

    Implement App component with alert and conditional logic
    Added an App component with alert and conditional rendering.

--- Function.jsx
+++ Function.jsx
@@ -10,3 +10,42 @@ const Function = () => {
 export default Function;
 
 -----------ekhane Function Hocce akta component--------------------
+
+========================Aleart Function==================
+    const App = () => {
+
+  return (
+   <div>
+     <button onClick={()=>alert("I am Lutfor Rhaman")}>Submit</button>
+   </div>
+  );
+};
+
+================================Basic Text/ Css Use====================
+    const App = () => {
+
+  return (
+   <div style={{ textAlign: 'center' }}>
+     <h1 style={
+      {color:"red"}
+     }>{2+2}</h1>
+     <About/>
+     <Function/>
+
+   </div>
+  );
+};
+
+export default App;
+
+========================================Shoart Hand If else==============================
+const Function = () => {
+    const mark = 70;
+    return (
+        <div>
+            { mark>=80 ? <h2>Briliant Result</h2> : <h2>Avarage Result</h2> }
+        </div>
+    );
+};
+export default Function;
+    
