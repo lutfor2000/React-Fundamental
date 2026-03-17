da3aca6bef1be10defecd900d79d808a109c9847
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-03-17 23:19:48 +0600

    Refactor LoginCondition with switch statement
    Refactor LoginCondition component to use switch statement for handling login status.

--- LoginCondition.jsx
+++ LoginCondition.jsx
@@ -1,1 +1,30 @@
-L
+==================================use if Conditon==============================
+import React from 'react';
+
+const status = false;
+
+const LoginCondition = () => {
+      if (status == true) {
+         return <button>Login Success !</button>
+      }else{
+         return <span>Logout</span>
+      }
+};
+
+export default LoginCondition;
+
+===========================================use switch condition=============================
+const LoginCondition = () => {
+    const status = true;
+    
+    switch (status) {
+        case true:
+            return <button>Login successfull</button>
+        case false:
+            return <button>Login</button>
+        default:
+            return <h1>this null Value</h1>;
+    }
+};
+
+export default LoginCondition;
