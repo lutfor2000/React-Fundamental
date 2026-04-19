5c05776d66d140d9620fb6f652e8e707d81313d0
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-04-19 18:28:28 +0600

    Update React-Hook.jsx

--- React-Hook.jsx
+++ React-Hook.jsx
@@ -1,1 +1,20 @@
+01. React Hook is a feature in the React library that allows developers to use state and other React features in functional components, which were previously only available in class components.
+
+02. It was introduced in React version 16.8.
+
+03. Hooks can be used to manage state, handle si ffects, and access lifecycle methods in functional components.
+
+04. There are several built-in Hooks provided by React, such as useState, useEffect, useContext, and useRef.
+
+05. React Hooks have greatly simplified the development process in React and have made it easier to write reusable and composable code.
+
+০১. রিয়্যাক্ট হুক হলো রিয়্যাক্ট লাইব্রেরির একটি ফিচার যা ডেভেলপারদেরকে ফাংশনাল কম্পোনেন্টে স্টেট এবং অন্যান্য রিয়্যাক্ট ফিচার ব্যবহার করার সুযোগ দেয়, যা আগে শুধুমাত্র ক্লাস কম্পোনেন্টেই পাওয়া যেত।
+
+০২. এটি রিয়্যাক্ট ভার্সন ১৬.৮-এ চালু করা হয়েছিল।
+
+০৩. ফাংশনাল কম্পোনেন্টে স্টেট পরিচালনা, ইফেক্ট হ্যান্ডেল করা এবং লাইফসাইকেল মেথড অ্যাক্সেস করার জন্য হুক ব্যবহার করা যায়।
+
+০৪. রিয়্যাক্টের দেওয়া বেশ কিছু বিল্ট-ইন হুক রয়েছে, যেমন useState, useEffect, useContext, এবং useRef।
+
+০৫. রিয়্যাক্ট হুকস রিয়্যাক্টে ডেভেলপমেন্ট প্রক্রিয়াকে অনেক সহজ করে দিয়েছে এবং পুনঃব্যবহারযোগ্য ও কম্পোজেবল কোড লেখা আরও সহজ করে তুলেছে।
 
