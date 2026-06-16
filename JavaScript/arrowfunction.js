682e8c2e6bd54e7c003afedbb51304b9d0d09a33
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-06-16 23:32:58 +0600

    Update arrowfunction.js

--- JavaScript/arrowfunction.js
+++ JavaScript/arrowfunction.js
@@ -1,1 +1,15 @@
+const javascript = {
 
+    name: "JavaScript",
+    libraries: ["React","Angular","Vue"],
+
+    printLibaries: function(){
+        this.libraries.forEach((a)=>{
+
+            console.log(`${this.name} Love ${a}`)
+        })
+    }
+
+}
+
+javascript.printLibaries()
