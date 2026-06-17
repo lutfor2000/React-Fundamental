07e004b8a1918e83063f6fea2c4503e5ca67ae19
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-06-17 19:19:39 +0600

    Update truty-and-falsy-vule.js

--- JavaScript/truty-and-falsy-vule.js
+++ JavaScript/truty-and-falsy-vule.js
@@ -1,3 +1,9 @@
 // যে সব ভ্যালু false এর মতো তাকে falsy ভ্যালু বলে 
 
 falsy value => false, 0 ,-0, 0n (BigInt zero) ,"" (empty string) ,null ,undefined ,NaN
+
+if(myVar){
+    console.log("I am Truthy Value");
+}else{
+    console.log("I am Falsy Value");
+}
