abda274ac4b4aeb286815e691298c43e997159af
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-06-17 19:31:47 +0600

    Update Ternay-operator.js

--- JavaScript/Ternay-operator.js
+++ JavaScript/Ternay-operator.js
@@ -1,1 +1,19 @@
+const age = 20
+let type;
+
+if(age >= 18){
+    type = "Adult"
+}else{
+    type = "Child"
+}
+
+console.log(type)
+
+//--Ternary Operator
+const age = 10
+
+const rasult = age >= 18 ? "Adult" : age >= 7 ? "Infant" : "Child";
+
+console.log(rasult)
+
 
