d53424272d0d5a5efc2475c0e5d5eb5a94718e7c
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-06-17 22:06:16 +0600

    Update find_arow.js

--- JavaScript/find_arow.js
+++ JavaScript/find_arow.js
@@ -4,3 +4,10 @@ const arrowList = [1,2,3,4,5,6];
 const resut = arrowList.find((currentvelu) => currentvelu === 3);
 
 console.log(resut)
+
+//---Index Number check
+const numbers = [1,2,3,4,5,6,7,8,9,10,11];
+
+const result = numbers.findIndex((currentValue) => currentValue > 4)
+
+console.log(result);
