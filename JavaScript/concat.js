ef4053a1832622695b6503c5e82c5e7bc7a563bb
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-06-19 01:06:17 +0600

    Create concat.js

---
+++ JavaScript/concat.js
@@ -0,0 +1,8 @@
+const number01 = [1,2,3,4,5,6,7,8,9,10,11];
+const number02 = [14,15,16]
+const number03 = [100,200,300]
+
+
+const result = number01.concat(number02,number03);
+
+console.log(result);
