48f49f0bb06ea39652a6719b070b4f3745972777
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-06-19 00:54:10 +0600

    Create splice.js

---
+++ JavaScript/splice.js
@@ -0,0 +1,9 @@
+const numbers = [1,2,3,4,5,6,7,8,9,10,11];
+
+const result = numbers.splice(3,4,12,13)
+
+console.log(result);
+
+console.log(numbers)
+
+//--array.splice(start, deleteCount, newItems...)--
