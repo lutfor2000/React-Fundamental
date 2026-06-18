3db59f2378c5524ff160e4da38206afca489715e
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-06-19 00:54:37 +0600

    Update splice.js

--- JavaScript/splice.js
+++ JavaScript/splice.js
@@ -1,3 +1,5 @@
+//--array.splice(start, deleteCount, newItems...)--
+
 const numbers = [1,2,3,4,5,6,7,8,9,10,11];
 
 const result = numbers.splice(3,4,12,13)
@@ -6,4 +8,3 @@ console.log(result);
 
 console.log(numbers)
 
-//--array.splice(start, deleteCount, newItems...)--
