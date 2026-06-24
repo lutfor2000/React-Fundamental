e7e415812827e0d35b65177e8e7d31126eb8d77f
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-06-24 21:02:14 +0600

    Create for-loop.js

---
+++ JavaScript/for-loop.js
@@ -0,0 +1,10 @@
+//========Object Value Select================
+const myObj = {
+    name: "Java",
+    year: 1998,
+    founder: "Lutfor Rhaman"
+}
+
+for(proparty in myObj){
+    console.log(myObj[proparty]);
+}
