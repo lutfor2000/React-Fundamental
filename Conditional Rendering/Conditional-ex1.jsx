ebb4578853f02a6ad179b310d75fbfd6a4902ac9
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-08-26 22:23:48 +0600

    Create Conditional-ex1.jsx

---
+++ Conditional Rendering/Conditional-ex1.jsx
@@ -0,0 +1,32 @@
+const Item = ({name,isPacked})=>{
+
+    // if(isPacked){
+    //     return <li className="ml-10">Start : {name} ✅</li> 
+    // }
+
+    return(
+        <div className="w-80 m-auto mt-3">
+            <li className="ml-10 ">{ isPacked ? ( <del>{name + "✅"}</del>) : (<li className="bg-green-400">{name}</li>) }</li>
+        </div>
+
+
+    )
+    
+
+}
+
+
+//=====Next Page=========
+import Item from "./Item"
+
+const PakegList = () => {
+    return (
+        <div className="mt-10">
+            <Item isPacked={true} name="Lutfor Rhaman Shanto"/>
+            <Item isPacked={true} name="This is My Account"/>
+            <Item isPacked={false} name="This Account ok"/>
+        </div>
+    );
+};
+
+export default PakegList;
